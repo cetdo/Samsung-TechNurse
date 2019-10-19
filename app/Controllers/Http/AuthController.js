1951e21b1609d27e00497a4ebaf9f034490711bb
cetdo <ceto.snf17@uea.edu.br>
2019-10-19 15:30:27 -0400

    Subindo arquivos

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const User = use("App/Models/User");
+
+class AuthController {
+  /**
+   * Login
+   * Action login user in API
+   */
+
+  async registro({ view }) {
+    return view.render('register');
+  }
+
+  async login({view}) {
+    return view.render('login');
+  }
+
+  async login({ request, auth }) {
+    const { email, password } = request.all();
+    const token = await auth.attempt(email, password);
+    return token;
+  }
+
+  /**
+   * Store
+   * Action for user save into database
+   */
+  async store({ request }) {
+    const data = request.only(["email", "password"]);
+    const user = await User.create(data);
+    return user;
+  }
+
+  /**
+   * Profile
+   * Action for return authenticated user
+   */
+  async profile({ auth, response }) {
+    try {
+      return await auth.getUser();
+    } catch (error) {
+      response.send("You are not logged in");
+    }
+  }
+}
+
+module.exports = AuthController;
---
+++ start/routes.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use("Route");
+
+Route.get("/", ({ response }) => {
+  response.json({
+    name: "Technurse - API",
+    version: "v1",
+    visit: new Date(),
+    author: "Breno Pereira <breno.pereira@alumiar.me>"
+  });
+});
+
+
+Route.on("/registro").render("registro");
+
+Route.on("/login").render("login");
+
+Route.post("/api/auth/login", "AuthController.login");
+Route.post("/api/auth/register", "AuthController.store");
+
+Route.get("/api/nurses", "NurseController.index").middleware("auth");
+Route.get("/api/nurses/:id", "NurseController.show").middleware("auth");
+
+Route.get("/api/user", "AuthController.profile").middleware("auth");
